40be5643181338351455ad749d1ff2fdd58adbf1
Percival Li <p5li@uwaterloo.ca>
2025-05-08 17:05:29 +0800

    streak 1

--- Problems/58. Length of Last Word.cpp
+++ Problems/58. Length of Last Word.cpp
@@ -1,21 +1,21 @@
 //
 // Created by Percival on 2025/3/18.
 //
+#include <string>
+
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
+    int lengthOfLastWord(std::string s) {
         int ans = 0;
         bool counting = false;
-        for(int i = s.length() - 1; i >= 0; i--){
-          if(s[i] == ' '){
-            if (counting)
-              break;
-          }
-          else{
-            ans++;
-            counting = true;
-          }
-
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                if (counting)
+                    break;
+            } else {
+                ans++;
+                counting = true;
+            }
         }
         return ans;
     }
